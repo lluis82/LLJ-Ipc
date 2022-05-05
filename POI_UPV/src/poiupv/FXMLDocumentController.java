7b2100503fb216f9bc20dc045f6451dee95220c3
Estimp <javierchiquitin02@gmail.com>
2022-05-05 23:35:06 +0200

    2.0
    Primeros cambios en el zoom y limitación de la amplitud máxima de la lista de la izquierda

--- POI_UPV/src/poiupv/FXMLDocumentController.java
+++ POI_UPV/src/poiupv/FXMLDocumentController.java
@@ -141,9 +141,9 @@ public class FXMLDocumentController implements Initializable {
         initData();
         //==========================================================
         // inicializamos el slider y enlazamos con el zoom
-        zoom_slider.setMin(0.5);
-        zoom_slider.setMax(1.5);
-        zoom_slider.setValue(1.0);
+        zoom_slider.setMin(0.12);
+        zoom_slider.setMax(1.3);
+        zoom_slider.setValue(0.5);
         zoom_slider.valueProperty().addListener((o, oldVal, newVal) -> zoom((Double) newVal));
         
         //=========================================================================
